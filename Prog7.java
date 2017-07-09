1e2aa5ce802274066f7efac6cdedb95ede283da8
nikita <nikitaraghavan@gmail.com>
2017-07-09 13:27:53 +0530

    prog7

---
+++ Prog7.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Prog7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	
+        for(int i=1;i<=5;i++)
+        {
+        	System.out.println("Hello");
+        }
+    }
+		
+		
+}
